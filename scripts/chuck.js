e9bcc936cadfa3c30a1f789c8d0400d800e21bf0
Francesco Mondora <francesco.mondora@mondora.com>
2015-12-26 22:55:06 +0100

    Adding soul calendar

--- scripts/chuck.js
+++ scripts/chuck.js
@@ -1,5 +1,21 @@
-module.exports = function (chuck) {
+//var enterReplies = ["Hi", "I don’t do Burn Down charts, I prefer Smack Down charts.", "Firing", "Hello friend.", "Gotcha", "I see you"];
+//var leaveReplies = ["I don't need garbage collection because I don't call .Dispose(), I call .DropKick().", "Target lost", "Searching"];
 
+
+module.exports = function (chuck) {
+/*
+    chuck.listen (function (message) {
+        message.user.name("Makkina");
+    });
+
+    chuck.enter (function (res) {
+        res.send (res.random, enterReplies);
+    });
+
+    chuck.leave (function (res) {
+        res.send (res.random, leaveReplies);
+    });
+*/
     chuck.respond(/is it (weekend|holiday)\s?\?/i, function (msg) {
         var today = new Date();
         msg.reply(today.getDay() === 0 || today.getDay() === 6 ? "YES" : "NO");
